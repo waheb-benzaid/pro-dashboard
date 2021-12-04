a5a70c1f572d27bf9ab4a387146bb0cd1955d083
Wahéb <waheb.benzaid@gmail.com>
2021-12-04 23:57:34 +0100

    Add routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { NotesComponent } from './notes/notes.component';
+import { TodosComponent } from './todos/todos.component';
 
-const routes: Routes = [{ path: 'bookmarks', component: BookmarksComponent }];
+const routes: Routes = [
+  { path: 'bookmarks', component: BookmarksComponent },
+  { path: 'todos', component: TodosComponent },
+  { path: 'notes', component: NotesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
