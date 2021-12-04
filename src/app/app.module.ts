d81bf4a53a03b0d70e7b0cd07e0bf2febe566b34
Wahéb <waheb.benzaid@gmail.com>
2021-12-04 19:55:51 +0100

    Add needed component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { TodosComponent } from './todos/todos.component';
+import { NotesComponent } from './notes/notes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TabsComponent,
-    BookmarksComponent
+    BookmarksComponent,
+    TodosComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
